6338d46086c6865b909e9c09c0032755fd8256d2
Qunli <qunli02@gmail.com>
2019-11-19 22:51:59 -0500

    refactor

--- src/login.js
+++ src/login.js
@@ -323,10 +323,7 @@ class Login extends React.Component {
   moveAnywhere=(location,player)=>{
     fetch(`${API_ROOT}/api/v1/characters/${player.character.id}`, {
       method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
+      headers: HEADERS,
       body: JSON.stringify({...player.character,location:location, damage:null}),
     })
     this.setState({
